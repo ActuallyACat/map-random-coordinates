204c095a793dd429ab75d81836d28cea002b019d
Ally Beaton <allanna.beaton@gmail.com>
2022-08-07 18:47:44 +1000

    Consume api util, track api loading state, output marker

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,39 +1,72 @@
+import { LatLngBounds, LatLngBoundsExpression, LatLngTuple } from 'leaflet';
 import * as React from 'react'
 import { useState, useEffect } from 'react';
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import { CircleMarker, MapContainer, Marker, Popup, TileLayer, Tooltip, useMapEvents } from 'react-leaflet'
+import { post } from "./utils"
 
-interface AppProps {}
+interface AppProps { }
+enum LoadingState {
+	Loading,
+	Error,
+	Complete
+}
+
+const MapBounds = ({ setBounds }: { setBounds: React.Dispatch<React.SetStateAction<LatLngBounds | null>> }) => {
+	const map = useMapEvents({
+		moveend: () => {
+			const bottomLeft = map.getBounds().getNorthWest()
+			const topRight = map.getBounds().getSouthEast()
+			const bounds = new LatLngBounds(bottomLeft, topRight)
+			setBounds(bounds)
+		},
+	})
+
+	return null
+}
 
 const App = (props: AppProps) => {
-	const [greeting, setGreeting] = useState<string>('');
+	// @TODO: get inital bounds 
+	const [bounds, setBounds] = useState<LatLngBounds | null>(null);
+	const [apiState, setApiState] = useState<LoadingState>(LoadingState.Complete);
+	const [markers, setMarkers] = useState<[]>([]);
+	const initialPosition: LatLngTuple = [-32.93, 151.72]
 
 	useEffect(() => {
-		async function getGreeting() {
+		async function sendBounds() {
+			if (!bounds) return;
 			try {
-				const res = await fetch('/api/hello');
-				const greeting = await res.json();
-				setGreeting(greeting);
-			} catch (error) {
-				console.log(error);
+				// @TODO - better types for expected sent/received data
+				setApiState(LoadingState.Loading)
+				const response = await post<{ points: any }>('points', {
+					todo: 'test'
+				});
+				setApiState(LoadingState.Complete)
+				setMarkers(response.points)
+			} catch (err) {
+				setApiState(LoadingState.Error)
+				console.error(err)
 			}
 		}
-		getGreeting();
-	}, []);
+		sendBounds();
+	}, [bounds]);
 
 	return (
 		<main className="container my-5">
-			<h1 className="text-primary text-center">Hello {greeting}!</h1>
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
+			<MapContainer center={initialPosition} zoom={13} scrollWheelZoom={false}>
+				<TileLayer
+					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+				/>
+				<MapBounds setBounds={setBounds} />
+				{markers.map(({ lat, long }, i) => (
+					<CircleMarker
+						key={`marker-${i}`}
+						center={[lat, long]}
+						pathOptions={{ color: 'red' }}
+						radius={10}>
+					</CircleMarker>
+				)}
+			</MapContainer>
 		</main>
 	);
 };
