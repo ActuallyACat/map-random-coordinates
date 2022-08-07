aaafc900b582c5fb8a94e1ad14119b591c1d9b7d
Ally Beaton <allanna.beaton@gmail.com>
2022-08-07 21:03:26 +1000

    Updated types for Marker

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,34 +1,24 @@
-import { LatLngBounds, LatLngBoundsExpression, LatLngTuple } from 'leaflet';
+import { LatLngBounds, LatLngTuple } from 'leaflet';
 import * as React from 'react'
 import { useState, useEffect } from 'react';
-import { CircleMarker, MapContainer, Marker, Popup, TileLayer, Tooltip, useMapEvents } from 'react-leaflet'
+import { CircleMarker, MapContainer, TileLayer } from 'react-leaflet'
 import { post } from "./utils"
+import { UpdateMapBoundaryBox } from "./components/UpdateMapBoundaryBox"
 
 interface AppProps { }
+
 enum LoadingState {
 	Loading,
 	Error,
 	Complete
 }
-
-const MapBounds = ({ setBounds }: { setBounds: React.Dispatch<React.SetStateAction<LatLngBounds | null>> }) => {
-	const map = useMapEvents({
-		moveend: () => {
-			const bottomLeft = map.getBounds().getNorthWest()
-			const topRight = map.getBounds().getSouthEast()
-			const bounds = new LatLngBounds(bottomLeft, topRight)
-			setBounds(bounds)
-		},
-	})
-
-	return null
-}
+type Marker = { lat: number, lng: number }
 
 const App = (props: AppProps) => {
 	// @TODO: get inital bounds 
 	const [bounds, setBounds] = useState<LatLngBounds | null>(null);
 	const [apiState, setApiState] = useState<LoadingState>(LoadingState.Complete);
-	const [markers, setMarkers] = useState<[]>([]);
+	const [markers, setMarkers] = useState<Marker[]>([]);
 	const initialPosition: LatLngTuple = [-32.93, 151.72]
 
 	useEffect(() => {
@@ -36,8 +26,7 @@ const App = (props: AppProps) => {
 			if (!bounds) return;
 			setApiState(LoadingState.Loading)
 			try {
-				// @TODO - better types for expected sent/received data
-				post<{ points: any }>('points', {
+				post<{ points: Marker[] }>('points', {
 					topLeft: bounds.getNorthWest(),
 					topRight: bounds.getNorthEast(),
 					bottomRight: bounds.getSouthEast(),
@@ -59,11 +48,11 @@ const App = (props: AppProps) => {
 		<main className="container my-5">
 			<h1>{apiState === 1 && <h1>Loading</h1>}</h1>
 			<MapContainer center={initialPosition} zoom={13} scrollWheelZoom={false}>
+				<UpdateMapBoundaryBox setBounds={setBounds} />
 				<TileLayer
 					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 				/>
-				<MapBounds setBounds={setBounds} />
 				{markers.map(({ lat, lng }, i) => (
 					<CircleMarker
 						key={`marker-${i}`}
@@ -71,7 +60,7 @@ const App = (props: AppProps) => {
 						pathOptions={{ color: 'red' }}
 						radius={10}>
 					</CircleMarker>
-				)}
+				))}
 			</MapContainer>
 		</main>
 	);
